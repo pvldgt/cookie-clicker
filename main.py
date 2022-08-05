80f275bb122a806ca3fc975746cc5b1546b87896
Pavel Degtyarev <pvldgt@gmail.com>
2022-08-05 08:44:58 +0300

    find the prices of available items

--- main.py
+++ main.py
@@ -35,13 +35,29 @@ import time
 # Click on the cookie
 # make the while loop work for 5 minutes
 end_time = time.time() + 60 * 5
-print(time.time())
-print(end_time)
+
+# list_of_prices = []
+
 while time.time() < end_time:
+    # Find the cookie element
     cookie = driver.find_element(By.ID, "bigCookie")
+    # Click on it
     cookie.click()
+    # Delay of 1 sec between clicks for ease of development
+    time.sleep(1)
 
+    # Create a list of prices of available items
+    list_of_prices = []
+    try:
+        prices_available = driver.find_elements(By.CSS_SELECTOR, "div.product.unlocked.enabled div.content span.price")
+        for price in prices_available:
+            if int(price.text) not in list_of_prices:
+                list_of_prices.append(int(price.text))
+    except NoSuchElementException:
+        print("no such element")
 
+    # Print the prices of available items
+    print(list_of_prices)
 
 
 
